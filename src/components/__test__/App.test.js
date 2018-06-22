b08495845602ffb3f52570855086618cfce21ff7
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-06-21 22:51:25 -0300

    faking the App component renderization with JSDOM

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,5 +4,10 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
+    const div = document.create('div');
+
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+
     
 });
