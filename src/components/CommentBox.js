2e6541b1a67218e532182d69b0a547e553e2ef3d
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-06-24 21:41:14 -0300

    enhancing the CommentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            comment : ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ comment : event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ comment : '' });
+    }
     
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h4>Add a Comment</h4>
-                <textarea />
+                <textarea value={this.state.comment} onChange={this.handleChange} />
 
                 <div>
                     <button>Submit Comment</button>
