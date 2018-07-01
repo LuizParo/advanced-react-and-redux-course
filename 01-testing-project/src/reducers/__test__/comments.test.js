0069a2d8cc2e62c86283ad87d9e13b067bd1e4ed
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-01 19:39:07 -0300

    creating the scaffold of the comments integration test and fixing broken tests

--- 01-testing-project/src/reducers/__test__/comments.test.js
+++ 01-testing-project/src/reducers/__test__/comments.test.js
@@ -1,15 +1,21 @@
 import commentsReducer from 'reducers/comments';
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 const NEW_COMMENT = 'New Comment';
 
 it('handles actions of type SAVE_COMMENT', () => {
-    const action = {
-        type : SAVE_COMMENT,
-        payload : NEW_COMMENT
-    };
-
+    const action = createAction(SAVE_COMMENT, NEW_COMMENT);
     const newState = commentsReducer([], action);
+
+    expect(newState).toEqual([NEW_COMMENT]);
+});
+
+it('handles actions of type FETCH_COMMENTS', () => {
+    const action = createAction(FETCH_COMMENTS, {
+        data : [{ name : NEW_COMMENT }]
+    });
+    const newState = commentsReducer([], action);
+
     expect(newState).toEqual([NEW_COMMENT]);
 });
 
@@ -17,3 +23,10 @@ it('handles action with unknown type', () => {
     const newState = commentsReducer([], { type : 'unknown' });
     expect(newState).toEqual([]);
 });
+
+function createAction(type, payload) {
+    return {
+        type,
+        payload
+    };
+}
