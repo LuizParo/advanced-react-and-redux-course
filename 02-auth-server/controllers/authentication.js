794359bc401ae28408bca7bd2b0c79fcbcf539cd
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-12 17:43:48 -0300

    creating new user if it does not exist in the database

--- 02-auth-server/controllers/authentication.js
+++ 02-auth-server/controllers/authentication.js
@@ -5,12 +5,28 @@ exports.signup = (req, res, next) => {
 
     // See if a user with the given email exists.
     User.findOne({ email }, (err, existingUser) => {
+        if (err) {
+            return next(err);
+        }
 
+        // If a user with email does exist, return an error.
+        if (existingUser) {
+            return res.status(422).send({ error : 'Email is in use' });
+        }
+
+        // If a user which email does NOT exist, create and save user record.
+        const newUser = new User({
+            email,
+            password
+        });
+
+        newUser.save(err => {
+            if (err) {
+                next(err);
+            }
+
+            // Respond to request indicating the user was created.
+            res.status(201).json({ success : true });
+        });
     });
-
-    // If a user with email does exist, return an error.
-
-    // If a user which email does NOT exist, create and save user record.
-
-    // Respond to request indicating the user was created.
 };
