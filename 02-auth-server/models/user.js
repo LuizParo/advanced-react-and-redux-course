a87f71c0ccc7e7c4b2715e501534a29d1ead2438
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-12 18:17:09 -0300

    fixing bugs while encrypting the user's password

--- 02-auth-server/models/user.js
+++ 02-auth-server/models/user.js
@@ -13,19 +13,24 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
-userSchema.pre('save', next => {
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+    // get access to the user model
     const user = this;
 
+    // generate a salt then run callback
     bcrypt.genSalt(10, (err, salt) => {
         if (err) {
             return next(err);
         }
 
-        bcrypt.hash(user.password, salt, (err, hash) => {
+        // hash (encrypt) our password using the salt
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
             if (err) {
                 return next(err);
             }
 
+            // overwrite plain text password with encrypted password
             user.password = hash;
             next();
         });
