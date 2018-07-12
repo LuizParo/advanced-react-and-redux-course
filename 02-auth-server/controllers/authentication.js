51d43ee467a10b00ce08d04ba702d9328386873c
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-12 13:43:10 -0300

    using mongoose to search the users by their email address

--- 02-auth-server/controllers/authentication.js
+++ 02-auth-server/controllers/authentication.js
@@ -1,4 +1,16 @@
-exports.signup = (req, res, next) => {
+const User = require('../models/user');
 
-    res.send({ success : true });
+exports.signup = (req, res, next) => {
+    const { email, password } = req.body;
+
+    // See if a user with the given email exists.
+    User.findOne({ email }, (err, existingUser) => {
+
+    });
+
+    // If a user with email does exist, return an error.
+
+    // If a user which email does NOT exist, create and save user record.
+
+    // Respond to request indicating the user was created.
 };
