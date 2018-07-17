04ed937aa66392a3da10ff856f22cd83da128759
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-17 12:05:41 -0300

    wiring up react router into the application

--- 03-auth-client/src/components/App.js
+++ 03-auth-client/src/components/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Header from './Header';
 
-const App = props => (
+const App = ({ children }) => (
     <div>
         <Header />
-        Hi, I'm the app
+        {children}
     </div>
 );
 
---
+++ 03-auth-client/src/components/Welcome.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default _ => <h3>Welcome! Sign Up or Sign In!</h3>;
