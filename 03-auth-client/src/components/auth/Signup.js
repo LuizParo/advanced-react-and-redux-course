b561e1541b81b6b8b0865155d613e1418b8a7fbb
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-18 13:35:02 -0300

    wiring up redux-form inside Signup component

--- 03-auth-client/src/components/auth/Signup.js
+++ 03-auth-client/src/components/auth/Signup.js
@@ -3,9 +3,15 @@ import { reduxForm, Field } from 'redux-form';
 
 class Signup extends Component {
 
+    onSubmit = formProps => {
+        console.log(formProps);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
                 <fieldset>
                     <label>Email</label>
                     <Field name="email" type="text" component="input" autoComplete="none" />
@@ -15,6 +21,8 @@ class Signup extends Component {
                     <label>Password</label>
                     <Field name="password" type="password" component="input" autoComplete="none" />
                 </fieldset>
+
+                <button>Sign Up!</button>
             </form>
         );
     }
