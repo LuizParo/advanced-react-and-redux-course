0a41e33b9456af9277a3f8718136564cfbd0f9d9
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-20 11:29:15 -0300

    displaying error messages if the signup request fails

--- 03-auth-client/src/actions/index.js
+++ 03-auth-client/src/actions/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-import { AUTH_USER } from "./types";
+import { AUTH_ERROR, AUTH_USER } from "./types";
 
 export const signup = formProps => async dispatch => {
-    const response = await axios.post(`http://localhost:3090/signup`, formProps);
-
-    dispatch({ type : AUTH_USER, payload : response.data.token });
+    try {
+        const response = await axios.post(`http://localhost:3090/signup`, formProps);
+        dispatch({ type : AUTH_USER, payload : response.data.token });
+    } catch (error) {
+        dispatch({ type : AUTH_ERROR, payload : error.response.data.error });
+    }
 };
--- 03-auth-client/src/components/auth/Signup.js
+++ 03-auth-client/src/components/auth/Signup.js
@@ -26,13 +26,23 @@ class Signup extends Component {
                     <Field name="password" type="password" component="input" autoComplete="none" />
                 </fieldset>
 
+                <div>
+                    {this.props.errorMessage}
+                </div>
+
                 <button>Sign Up!</button>
             </form>
         );
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        errorMessage : state.auth.errorMessage
+    };
+}
+
 export default compose(
-    connect(null, actions),
+    connect(mapStateToProps, actions),
     reduxForm({ form : 'signup' })
 )(Signup);
